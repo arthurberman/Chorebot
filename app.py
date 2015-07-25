b558c2728e9cc9c88a40833d3d674f2a42bd517a
Arthur Berman <aberman@Arthurs-MacBook-Air.local>
2015-07-25 13:53:57 -0700

    added dummy get

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
+
+chore = {"name":"Take out the trash", "recurring":False, "frequency":None}
+demochore = {"chores":[
+        {"priority":5, "active":True, "user":None, "chore":chore}
+        ]}
+
+
 @app.route('/')
 def index():
     return "Chores"
 
+@app.route('/chores.json')
+def chores():
+    return jsonify(demochore)
+
 if __name__=="__main__":
-    app.run()
+    app.run(port=5000, debug=True)
